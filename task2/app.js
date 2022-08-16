afbf77012eb8c804a76a5d780c873a96317cc279
vinayak1809 <vinayakrshinde18@gmail.com>
2022-08-16 16:35:56 +0530

    changes

--- task2/app.js
+++ task2/app.js
@@ -1,15 +1,23 @@
 // const http = require("http"); //node
 const express = require("express"); // third-party express
-const app = express();
+const bodyParser = require("body-parser");
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' value='product' name='product'/><input type='text' value='price' name='price'/><button>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("next to first middleware");
-  res.send({ key1: "value" });
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express JS</h1>");
 });
 
 app.listen(4000);
