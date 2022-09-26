df68e43539977d6966d19a901afe58c3b4eef225
vinayak1809 <vinayakrshinde18@gmail.com>
2022-09-27 02:15:31 +0530

    file upload

--- 00-starting-setup/controllers/error404.js
+++
@@ -1,3 +0,0 @@
-exports.error404 = (req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
-};
--- task2/controller/shopController.js
+++
@@ -1,7 +0,0 @@
-exports.contactUs = (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "contactus.html"));
-};
-
-exports.success = (req, res, next) => {
-  res.send("<h1>Form successfuly filled</h1>");
-};
