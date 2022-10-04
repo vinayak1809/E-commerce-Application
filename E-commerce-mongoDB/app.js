07928ad4bd200a37248f1d344bda0b6dfd7c92af
vinayak1809 <vinayakrshinde18@gmail.com>
2022-10-04 12:00:02 +0530

    user table

--- E-commerce-mongoDB/app.js
+++ E-commerce-mongoDB/app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 dotenv.config();
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  //User.findById(1)
-  //  .then((user) => {
-  //    req.user = user;
-  //    next();
-  //  })
-  //  .catch((err) => console.log(err));
+  User.findUserById(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
   next();
 });
 
--- E-commerce-mongoDB/models/user.js
+++ E-commerce-mongoDB/models/user.js
@@ -1,17 +1,39 @@
-//const Sequelize = require('sequelize');
-//
-//const sequelize = require('../util/database');
-//
-//const User = sequelize.define('user', {
-//  id: {
-//    type: Sequelize.INTEGER,
-//    autoIncrement: true,
-//    allowNull: false,
-//    primaryKey: true
-//  },
-//  name: Sequelize.STRING,
-//  email: Sequelize.STRING
-//});
-//
-//module.exports = User;
-//
+const mongoDB = require("mongodb");
+const getDb = require("../util/database").getDb;
+
+class User {
+  constructor(id, name, email, password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  saveUser() {
+    const db = getDb();
+    let dbOp;
+    return db
+      .collection("user")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result, "account created");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  findUserById(userId) {
+    const db = getDb();
+    let dbOp;
+    return db
+      .collection("user")
+      .findOne({ _id: new mongoDB.ObjectId(userId) })
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+module.exports = User;
