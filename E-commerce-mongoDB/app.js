e15f71a39025785fc8fcc90f49481c49b105646c
vinayak1809 <vinayakrshinde18@gmail.com>
2022-10-06 14:07:34 +0530

    cart system

--- E-commerce-mongoDB/app.js
+++ E-commerce-mongoDB/app.js
@@ -21,13 +21,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findUserById(1)
+  User.findUserById("633c487ff10bc8532b8a89bd")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, "", user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
-  next();
 });
 
 app.use("/admin", adminRoutes);
